c5afd573e557de8951a9f196f9702baad59c6642
developercodingarrow <digitalcloudinfo@gmail.com>
2025-09-22 09:54:00 +0530

    change enquire authntication

--- routes/enquireRoute.js
+++ routes/enquireRoute.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 const enquireController = require("../controllers/enquireController");
 const authController = require("../controllers/authController");
-router.use(
-  authController.protect,
-  authController.restricTO("superAdmin", "admin")
-);
+// router.use(
+//   authController.protect,
+//   authController.restricTO("superAdmin", "admin")
+// );
 
 router.post("/createEnquire", enquireController.createEnquiry);
-router.use(authController.protect, authController.restricTO("superAdmin"));
+// router.use(authController.protect, authController.restricTO("superAdmin"));
 router.get("/allEnquire", enquireController.getAllEnquiries);
 
 module.exports = router;
